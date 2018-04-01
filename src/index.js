e07e2796d7e430e994e6ca7f4a20c850eba016c1
BassXiaoSen <314121378@qq.com>
2018-04-01 14:50:52 +0800

    引入material-ui，引入PropTypes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,43 @@
 import React from "react"
 import ReactDom from "react-dom"
-import "./index.css"
-import Extra from "./Extra"
+import getMuiTheme from "material-ui/styles/getMuiTheme"
+import RaisedButton from "material-ui/RaisedButton"
+import PropTypes from "prop-types"
 
-class App extends React.Component{
-    render(){
-        return(
-            <div>Hello 我是React组件<Extra/></div>
+import injectTapEventPlugin from "react-tap-event-plugin"
+injectTapEventPlugin()
+
+
+class App extends React.Component {
+    getChildContext() {
+        return { muiTheme: getMuiTheme() }
+    }
+
+    getStyles() {
+        const styles = {
+            root: {
+                paddingTop: '50px',
+                minHeight: '400px'
+            }
+        }
+        return styles
+    }
+
+    render() {
+        const styles = this.getStyles()
+        return (
+            <div style={styles.root}>
+                <RaisedButton 
+                    label="Redux博客"
+                    primary={true}
+                />
+            </div>
         )
     }
 }
 
-ReactDom.render(<App/>,document.getElementById('root'))
+App.childContextTypes={
+    muiTheme:PropTypes.object.isRequired
+}
+
+ReactDom.render(<App />, document.getElementById('root'))
