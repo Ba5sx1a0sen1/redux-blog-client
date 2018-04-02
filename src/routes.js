fe3906da91a2f41c33e76a3c6a66c6d8917a88cb
BassXiaoSen <314121378@qq.com>
2018-04-02 16:00:42 +0800

    在route.js中引入react-redux，使用Provider组件传入store

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,20 @@
 import React from "react"
-import { Router, Route, browserHistory,hashHistory } from "react-router"
+import { Router, Route, browserHistory, hashHistory } from "react-router"
+import { Provider } from "react-redux"
 import App from "./components/App"
 import Login from "./components/auth/Login"
+import { store } from "./redux/store"
 
-export default class RenderRouter extends React.Component{
-    render(){
+export default class RenderRouter extends React.Component {
+    render() {
         return (
-            <Router history={browserHistory}>
-                <Route path="/" component={App}>
-                    <Route path="/login" component={Login} />
-                </Route>
-            </Router>
+            <Provider store={store}>
+                <Router history={browserHistory}>
+                    <Route path="/" component={App}>
+                        <Route path="/login" component={Login} />
+                    </Route>
+                </Router>
+            </Provider>
         )
     }
 }
